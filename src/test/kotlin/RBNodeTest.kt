b74b9c3c39ba0cebeb0cf5341e499679c053c80d
VersusXX <sultanovmuhammet.dev@gmail.com>
2024-03-30 04:58:05 +0300

    add: full cover RBNodeTest and RBTreeTest 30% covered
    Covered with tests toString method of RBNode.

--- src/test/kotlin/RBNodeTest.kt
+++ src/test/kotlin/RBNodeTest.kt
@@ -3,6 +3,18 @@ import org.junit.jupiter.api.Test
 class RBNodeTest {
 
     @Test
+    fun `max should return if empty`() {
+        val tree = RBTree<Int, String>()
+        assert(tree.max()?.value == null)
+    }
+
+    @Test
+    fun `min should return if empty`() {
+        val tree = RBTree<Int, String>()
+        assert(tree.min()?.value == null)
+    }
+
+    @Test
     fun `max should return max value`() {
         val tree = RBTree<Int, String>()
         tree.add(1, "A")
@@ -34,6 +46,33 @@ class RBNodeTest {
         tree.add(10, "J")
 
         assert(tree.min()?.value == "A")
+    }
+
+    @Test
+    fun `should return null if tree is empty`() {
+        val tree = RBTree<Int, String>()
+        assert(tree.root?.toString() == null)
+    }
+
+    @Test
+    fun `should return node with black color`() {
+        val tree = RBTree<Int, String>()
+        tree.add(1, "A")
+        tree.add(2, "B")
+        tree.add(3, "C")
+
+        assert(tree.getNode(1)?.toString() == "(1: A)")
+    }
+
+    @Test
+    fun `should return node with red color`() {
+        val tree = RBTree<Int, String>()
+        tree.add(1, "A")
+        tree.add(2, "B")
+        tree.add(3, "C")
+        tree.setColored(true)
+
+        assert(tree.getNode(1)?.toString() == "\u001B[31m(1: A)\u001B[0m")
     }
 
 }
